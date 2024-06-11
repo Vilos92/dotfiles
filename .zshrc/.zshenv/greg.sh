bc32b9e58506de8cf5566244d73983809d8b67e8
Gregory Linscheid <linscheid.greg@gmail.com>
2024-06-10 22:32:00 -0700

    :wrench: Additional aliases

--- .zshrc/.zshenv/greg.sh
+++ .zshrc/.zshenv/greg.sh
@@ -1,5 +1,17 @@
 #!/bin/zsh
 
-# Greg
-alias ssh-sparkify="ssh root@147.182.226.122"
+# Dotfiles
+alias gcddotfiles='cd ~/greg_projects/dotfiles'
+alias gtmuxdotfiles='gcddotfiles && tmux new-session -A -s dotfiles'
+
+# Milo Engine
+alias gcdmiloengine='cd ~/greg_projects/milo-engine'
+alias gtmuxmiloengine='gcdmiloengine && tmux new-session -A -s miloengine'
+
+# Sparkify
+alias gcdsparkify='cd ~/greg_projects/sparkify'
+alias gtmuxsparkify='gcdsparkify && tmux new-session -A -s sparkify'
+alias ssh-sparkify='ssh root@147.182.226.122'
+
+# Black magic
 source ${HOME}/.ghcup/env
