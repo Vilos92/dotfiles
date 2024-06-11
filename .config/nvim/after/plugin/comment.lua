cf0d8202cc1509928f18ff07778bb042285f916b
Gregory Linscheid <linscheid.greg@gmail.com>
2024-06-10 22:47:45 -0700

    :wrench: Cleanup

--- .config/nvim/after/plugin/comment.lua
+++ .config/nvim/after/plugin/comment.lua
@@ -4,16 +4,16 @@ local esc = vim.api.nvim_replace_termcodes(
   "<ESC>", true, false, true
 )
 
--- Toggle current line
+-- Toggle current line.
 vim.keymap.set("n", "<C-/>", function() commentApi.toggle.linewise.current() end, { noremap = true, silent = true })
 
--- Toggle selection (linewise)
+-- Toggle selection (linewise).
 vim.keymap.set("x", "<C-/>", function()
   vim.api.nvim_feedkeys(esc, "nx", false)
   commentApi.toggle.linewise(vim.fn.visualmode())
 end)
 
--- Toggle selection (blockwise)
+-- Toggle selection (blockwise).
 vim.keymap.set("x", "<C-'>", function()
   vim.api.nvim_feedkeys(esc, "nx", false)
   commentApi.toggle.blockwise(vim.fn.visualmode())
--- .config/nvim/after/plugin/explorer.lua
+++ .config/nvim/after/plugin/explorer.lua
@@ -1,14 +1,14 @@
--- disable netrw at the very start of your init.lua
+-- disable netrw at the very start of your 'init.lua'
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- optionally enable 24-bit colour
+-- optionally enable 24-bit colour.
 vim.opt.termguicolors = true
 
--- empty setup using defaults
+-- empty setup using defaults.
 require("nvim-tree").setup()
 
--- OR setup with some options
+-- OR setup with some options.
 require("nvim-tree").setup({
   sort = {
     sorter = "case_sensitive",
@@ -24,4 +24,4 @@ require("nvim-tree").setup({
   },
 })
 
-vim.keymap.set('n', '<C-b>', vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<C-b>", vim.cmd.NvimTreeToggle)
