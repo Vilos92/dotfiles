40407f8a1393c8eab7d62d97203812306b369856
Gregory Linscheid <linscheid.greg@gmail.com>
2024-06-12 00:39:27 -0700

    :wrench: Better tmux handling

--- zsh/.zshenv/pre/tmux.sh
+++ zsh/.zshenv/pre/tmux.sh
@@ -1,6 +1,13 @@
 attach_tmux_session() {
   if [ -z "$TMUX" ]; then
-    # Not inside a tmux session, create a new attached session.
+    # Not inside a tmux session.
+    
+    # If a second parameter is passed, change to that directory.
+    if [ -n "$2" ]; then
+      cd "$2"
+    fi 
+
+    # Create a new session and attach.
     tmux new-session -A -s "$1"
   else
     # Inside a tmux session, create a new detached session.
