0ab92ab3ed237734b87cb84669b3e318a417831d
Gregory Linscheid <linscheid.greg@gmail.com>
2024-06-14 14:59:59 -0700

    :wrench: Improved comment bindings

--- .config/nvim/after/plugin/comment.lua
+++ .config/nvim/after/plugin/comment.lua
@@ -4,18 +4,24 @@ local esc = vim.api.nvim_replace_termcodes(
   "<ESC>", true, false, true
 )
 
--- Toggle current line.
+-- Toggle current line
 vim.keymap.set("n", "<C-/>", function() commentApi.toggle.linewise.current() end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>/", function() commentApi.toggle.linewise.current() end, { noremap = true, silent = true })
 
--- Toggle selection (linewise).
+-- Toggle selection (linewise)
 vim.keymap.set("x", "<C-/>", function()
   vim.api.nvim_feedkeys(esc, "nx", false)
   commentApi.toggle.linewise(vim.fn.visualmode())
 end)
+vim.keymap.set("x", "<leader>/", function()
+  commentApi.toggle.linewise(vim.fn.visualmode())
+end)
 
--- Toggle selection (blockwise).
+-- Toggle selection (blockwise)
 vim.keymap.set("x", "<C-'>", function()
   vim.api.nvim_feedkeys(esc, "nx", false)
   commentApi.toggle.blockwise(vim.fn.visualmode())
 end)
-
+vim.keymap.set("x", "<leader>'", function()
+  commentApi.toggle.blockwise(vim.fn.visualmode())
+end)
