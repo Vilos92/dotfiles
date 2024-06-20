788f7f4ce8ced2956cc13e3cda4066a40982866d
Gregory Linscheid <linscheid.greg@gmail.com>
2024-06-19 21:54:26 -0700

    :wrench: Granular install scripts for your brews

--- scripts/brews.sh
+++ scripts/brews.sh
@@ -20,65 +20,191 @@ prompt_and_install() {
 }
 
 # Homebrew.
-prompt_and_install "homebrew" '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+install_homebrew() {
+  prompt_and_install "homebrew" '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+}
 
 # Alt tab window manager.
-prompt_and_install "alt-tab" 'brew install --cask alt-tab'
+install_alt_tab() {
+  prompt_and_install "alt-tab" 'brew install --cask alt-tab'
+}
 
 # Tiles snapping window manager.
-prompt_and_install "tiles" 'brew install --cask tiles'
+install_tiles() {
+  prompt_and_install "tiles" 'brew install --cask tiles'
+}
 
 # Terminal.
-prompt_and_install "alacritty" 'brew install --cask alacritty'
+install_alacritty() {
+  prompt_and_install "alacritty" 'brew install --cask alacritty'
+}
 
 # Developer environment packages.
-prompt_and_install "dev packages" 'brew install tmux\
-  zsh\
-  neovim\
-  stow\
-  ripgrep\
-  lua-language-server\
-  font-meslo-lg-nerd-font'
+install_dev_packages() {
+  prompt_and_install "dev packages" 'brew install tmux\
+    zsh\
+    neovim\
+    stow\
+    ripgrep\
+    lua-language-server\
+    font-meslo-lg-nerd-font'
+}
 
 # oh my zsh.
-prompt_and_install "oh my zsh" 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+install_oh_my_zsh() {
+  prompt_and_install "oh my zsh" 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+}
 
 # Powerlevel10k oh my zsh theme.
-prompt_and_install "powerlevel10k" 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k'
+install_powerlevel10k() {
+  prompt_and_install "powerlevel10k" 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k'
+}
 
 # Packer for neovim package management.
-prompt_and_install "packer.nvim" 'git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim'
+install_packer_nvim() {
+  prompt_and_install "packer.nvim" 'git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim'
+}
 
 # JavaScript/Node.js developement.
-prompt_and_install "JavaScript/Node.js packages" 'brew install fnm\
+install_javascript_packages() {
+  prompt_and_install "JavaScript packages" 'brew install fnm\
     oven-sh/bun/bun\
     fsouza/prettierd/prettierd &&
-  fnm install 22 &&
-  npm install -g eslint_d'
+    fnm install 22 &&
+    npm install -g eslint_d'
+}
 
 # Browser.
-prompt_and_install "arc" 'brew install --cask arc'
+install_arc() {
+  prompt_and_install "arc" 'brew install --cask arc'
+}
 
 # Notes.
-prompt_and_install "notion" 'brew install --cask notion'
+install_notion() {
+  prompt_and_install "notion" 'brew install --cask notion'
+}
 
 # Music.
-prompt_and_install "spotify" 'brew install --cask spotify'
+install_spotify() {
+  prompt_and_install "spotify" 'brew install --cask spotify'
+}
 
 # Archive manager.
-prompt_and_install "keka" 'brew install --cask keka'
+install_keka() {
+  prompt_and_install "keka" 'brew install --cask keka'
+}
 
 # Photo editor.
-prompt_and_install "gimp" 'brew install --cask gimp'
+install_gimp() {
+  prompt_and_install "gimp" 'brew install --cask gimp'
+}
 
 # Visual Studio Code.
-prompt_and_install "visual studio code" 'brew install --cask visual-studio-code@insiders'
+install_vscode() {
+  prompt_and_install "visual studio code" 'brew install --cask visual-studio-code@insiders'
+}
 
 # Text editor.
-prompt_and_install "coteditor" 'brew install --cask coteditor'
+install_coteditor() {
+  prompt_and_install "coteditor" 'brew install --cask coteditor'
+}
 
 # Database manager.
-prompt_and_install "dbeaver" 'brew install --cask dbeaver-community'
+install_dbeaver() {
+  prompt_and_install "dbeaver" 'brew install --cask dbeaver-community'
+}
 
 # Gif recorder.
-prompt_and_install "gifox" 'brew install --cask gifox'
+install_gifox() {
+  prompt_and_install "gifox" 'brew install --cask gifox'
+}
+
+handle_arguments() {
+  case $1 in
+    "homebrew" )
+      install_homebrew
+      ;;
+    "alt-tab" )
+      install_alt_tab
+      ;;
+    "tiles" )
+      install_tiles
+      ;;
+    "alacritty" )
+      install_alacritty
+      ;;
+    "dev-packages" )
+      install_dev_packages
+      ;;
+    "oh-my-zsh" )
+      install_oh_my_zsh
+      ;;
+    "powerlevel10k" )
+      install_powerlevel10k
+      ;;
+    "packer-nvim" )
+      install_packer_nvim
+      ;;
+    "javascript-packages" )
+      install_javascript_packages
+      ;;
+    "arc" )
+      install_arc
+      ;;
+    "notion" )
+      install_notion
+      ;;
+    "spotify" )
+      install_spotify
+      ;;
+    "keka" )
+      install_keka
+      ;;
+    "gimp" )
+      install_gimp
+      ;;
+    "vscode" )
+      install_vscode
+      ;;
+    "coteditor" )
+      install_coteditor
+      ;;
+    "dbeaver" )
+      install_dbeaver
+      ;;
+    "gifox" )
+      install_gifox
+      ;;
+    * )
+      echo "Invalid argument: $1"
+      exit 1
+      ;;
+  esac
+}
+
+install_everything() {
+  install_homebrew
+  install_alt_tab
+  install_tiles
+  install_alacritty
+  install_dev_packages
+  install_oh_my_zsh
+  install_powerlevel10k
+  install_packer_nvim
+  install_javascript_packages
+  install_arc
+  install_notion
+  install_spotify
+  install_keka
+  install_gimp
+  install_vscode
+  install_coteditor
+  install_dbeaver
+  install_gifox
+}
+
+if [ "$#" -eq 0 ]; then
+  install_everything
+else
+  handle_arguments $1
+fi
