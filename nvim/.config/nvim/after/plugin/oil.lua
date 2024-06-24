2d7e234b491a87d3a7b88e7427be00c959237530
Gregory Linscheid <linscheid.greg@gmail.com>
2024-06-24 14:32:48 -0700

    :wrench: More intuitive shortcuts

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -1,14 +1,16 @@
--- Leader based shortcut for opening Oil.
-vim.keymap.set("n", "<leader>pe", "<CMD>Oil<CR>", { desc = "Open parent directory" })
--- Alternative that mimics the VSCode "open explorer" shortcut.
-vim.keymap.set("n", "<C-b>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+-- Open Oil.
+vim.keymap.set("n", "<C-o>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Open Oil in a new tab.
 vim.keymap.set("n", "<C-t>", function()
   vim.cmd("tabnew | Oil")
 end)
 
--- Open Oil in a vertical split.
-vim.keymap.set("n", "<C-o>", function()
+-- Leader based shortcut for opening Oil in a split.
+vim.keymap.set("n", "<leader>pe", function()
   vim.cmd("rightbelow vsplit | Oil")
-end)
+end, { desc = "Open parent directory" })
+-- Alternative that mimics the VSCode "open explorer" shortcut.
+vim.keymap.set("n", "<C-b>", function()
+  vim.cmd("rightbelow vsplit | Oil")
+end, { desc = "Open parent directory" })
