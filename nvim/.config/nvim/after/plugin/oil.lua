a49a9d62d9eb4c89dd001b97b089c331cc12464b
Gregory Linscheid <linscheid.greg@gmail.com>
2024-06-24 15:01:46 -0700

    :wrench: Shorter syntax

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<C-o>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Open Oil in a new tab.
 vim.keymap.set("n", "<C-t>", function()
-  vim.cmd("tabe %:p:h | Oil")
+  vim.cmd("tabe %:p | Oil")
 end)
 
 -- Leader based shortcut for opening Oil in a split.
