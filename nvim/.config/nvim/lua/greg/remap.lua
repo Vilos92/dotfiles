780895ddd25529cd055f2e1c29b2cc13d5ec6a52
Gregory Linscheid <linscheid.greg@gmail.com>
2024-06-24 18:10:00 -0700

    :wrench: Ability to swap tab positions

--- nvim/.config/nvim/lua/greg/remap.lua
+++ nvim/.config/nvim/lua/greg/remap.lua
@@ -1,33 +1,36 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pq", vim.cmd.Ex)
 
--- Move lines up and down while respecting scope
+-- Move current tab left or right, using alt left and right.
+vim.keymap.set("n", "<M-left>", "<cmd>tabmove -1<CR>")
+vim.keymap.set("n", "<M-right>", "<cmd>tabmove +1<CR>")
+
+-- Move lines up and down while respecting scope.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- When deleting newline at end of line, keep cursor at beginning
+-- When deleting newline at end of line, keep cursor at beginning.
 vim.keymap.set("n", "J", "mzJ`z")
 
--- Keep cursor in place when navigating page up and down
+-- Keep cursor in place when navigating page up and down.
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
--- Keep cursor in middle when doing a local search
+-- Keep cursor in middle when doing a local search.
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Paste without replacing buffer with the overwritten content
+-- Paste without replacing buffer with the overwritten content.
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- Allow yanking content to the outer clipboard
+-- Allow yanking content to the outer clipboard.
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-
--- Allow replacing the currently highlighted term
+-- Allow replacing the currently highlighted term.
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
--- Make executable
+-- Make executable.
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Quick navigation.
