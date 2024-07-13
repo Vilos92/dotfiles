dfdc93647faa45fceb63220af32263e161105667
Gregory Linscheid <linscheid.greg@gmail.com>
2024-07-13 12:58:09 -0700

    :art: Color in fzf bat previews

--- zsh/.zshenv/pre/theme.sh
+++ zsh/.zshenv/pre/theme.sh
@@ -37,7 +37,7 @@ function alacritty-theme() {
 }
 
 function alacritty-theme-select() {
-  theme=$(ls $ALACRITTY_THEME_DIR_PATH | sed 's/\.toml$//g' | fzf --tmux --preview 'bat $ALACRITTY_THEME_DIR_PATH/{}.toml')
+  theme=$(ls $ALACRITTY_THEME_DIR_PATH | sed 's/\.toml$//g' | fzf --tmux --preview 'bat --color=always $ALACRITTY_THEME_DIR_PATH/{}.toml')
 
   if [ -z "$theme" ]; then
     echo "Theme not selected."
