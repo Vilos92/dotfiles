88b7d5dc6724554982305b0d5907d27adda096f1
Gregory Linscheid <linscheid.greg@gmail.com>
2024-07-12 20:39:30 -0700

    :mailbox: lualine

--- nvim/.config/nvim/lua/greg/plugins/filesystem.lua
+++ nvim/.config/nvim/lua/greg/plugins/filesystem.lua
@@ -2,6 +2,7 @@ return {
   -- Buffer-based filesystem editor
   {
     "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("oil").setup({
         view_options = {
@@ -15,7 +16,10 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     version = "0.1.6",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
     lazy = true,
   },
 
--- nvim/.config/nvim/lua/greg/plugins/tools.lua
+++ nvim/.config/nvim/lua/greg/plugins/tools.lua
@@ -1,4 +1,11 @@
 return {
+  -- Status line
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = true,
+  },
+
   -- Undo tree
   { "mbbill/undotree" },
 
