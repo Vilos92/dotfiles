c41e93ee4c140342f63ee11247933125b3917c01
Gregory Linscheid <linscheid.greg@gmail.com>
2024-07-15 23:58:01 -0700

    :wrench: Fuzzy history

--- zsh/.zshenv/post/util.sh
+++ zsh/.zshenv/post/util.sh
@@ -12,8 +12,21 @@ falias () {
   selection=$(echo "$aliases" | fzf --ansi --tmux 80%)
   [ -z "$selection" ] && return
 
-  echo "alias: $selection"
   eval $(echo "$selection" | awk -F= '{print $1}')
+}
+
+fuzzy-history () {
+  selection=$(
+    history |
+    awk '{$1=""; print substr($0,2)}' |
+    awk '!seen[$0]++' |
+    tail -r |
+    fzf
+  )
+
+  [ -z "$selection" ] && return
+
+  eval "$selection"
 }
 
 # tmux
