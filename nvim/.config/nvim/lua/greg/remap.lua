350e579a600687c634184b8a91c035a440982448
Gregory Linscheid <linscheid.greg@gmail.com>
2024-09-25 21:58:57 -0700

    :wrench: leader based pane handling

--- nvim/.config/nvim/lua/greg/remap.lua
+++ nvim/.config/nvim/lua/greg/remap.lua
@@ -9,16 +9,16 @@ vim.keymap.set("n", "<leader>[", "<cmd>tabmove -1<CR>")
 vim.keymap.set("n", "<leader>]", "<cmd>tabmove +1<CR>")
 
 -- Navigate panes using vim keys.
-vim.keymap.set("n", "<C-w>h", "<C-w>h")
-vim.keymap.set("n", "<C-w>j", "<C-w>j")
-vim.keymap.set("n", "<C-w>k", "<C-w>k")
-vim.keymap.set("n", "<C-w>l", "<C-w>l")
+vim.keymap.set("n", "<leader>bh", "<C-w>h")
+vim.keymap.set("n", "<leader>bj", "<C-w>j")
+vim.keymap.set("n", "<leader>bk", "<C-w>k")
+vim.keymap.set("n", "<leader>bl", "<C-w>l")
 
 -- Resize vim panes.
-vim.keymap.set("n", "<C-w>[", "5<C-w><")
-vim.keymap.set("n", "<C-w>]", "5<C-w>>")
-vim.keymap.set("n", "<C-w>-", "5<C-w>-")
-vim.keymap.set("n", "<C-w>=", "5<C-w>+")
+vim.keymap.set("n", "<leader>b[", "5<C-w><")
+vim.keymap.set("n", "<leader>b]", "5<C-w>>")
+vim.keymap.set("n", "<leader>b-", "5<C-w>-")
+vim.keymap.set("n", "<leader>b=", "5<C-w>+")
 
 -- Move lines up and down while respecting scope.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
