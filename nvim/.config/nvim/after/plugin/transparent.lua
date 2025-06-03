2cb09758b153fa309a6a7e21c3949abe5694afcd
Gregory Linscheid <linscheid.greg@gmail.com>
2025-06-03 15:52:24 -0700

    :art: Transparent

--- nvim/.config/nvim/after/plugin/transparent.lua
+++ nvim/.config/nvim/after/plugin/transparent.lua
@@ -32,6 +32,8 @@ transparent.setup({
     "StatusLineNC",
     "EndOfBuffer",
   },
-  extra_groups = {},
+  extra_groups = {
+    "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+  },
   exclude_groups = {},
 })
