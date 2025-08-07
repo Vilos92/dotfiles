1d6715f0596d539b1502e8ff5934d3bdbefdc5ea
Gregory Linscheid <linscheid.greg@gmail.com>
2025-08-07 16:09:42 -0700

    :shirt: Specify param for icons in eza

--- zsh/.zshenv/post/eza.sh
+++ zsh/.zshenv/post/eza.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Source: https://gist.github.com/AppleBoiy/04a249b6f64fd0fe1744aff759a0563b
-alias ls='eza --color=always --group-directories-first --icons'
-alias ll='eza -la --icons --octal-permissions --group-directories-first'
-alias l='eza -bGF --header --git --color=always --group-directories-first --icons'
-alias llm='eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons' 
+alias ls='eza --color=always --group-directories-first --icons=always'
+alias ll='eza -la --icons=always --octal-permissions --group-directories-first'
+alias l='eza -bGF --header --git --color=always --group-directories-first --icons=always'
+alias llm='eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons=always' 
 alias la='eza --long --all --group --group-directories-first'
-alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons'
+alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons=always'
 
-alias lS='eza -1 --color=always --group-directories-first --icons'
-alias lt='eza --tree --level=2 --color=always --group-directories-first --icons'
+alias lS='eza -1 --color=always --group-directories-first --icons=always'
+alias lt='eza --tree --level=2 --color=always --group-directories-first --icons=always'
 alias l.="eza -a | grep -E '^\.'"
