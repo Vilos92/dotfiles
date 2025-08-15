00ca2e19c77f4de7753489edfa44901601ee4c26
Gregory Linscheid <linscheid.greg@gmail.com>
2025-08-15 15:07:18 -0700

    :wrench: Simplify start.sh

--- docker/copyparty/start.sh
+++ docker/copyparty/start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Start copyparty in the background
 copyparty -c /app/copyparty.conf &
 
-# Wait a moment for copyparty to start
-sleep 2
-
-# Start cloudflared tunnel with correct origin URL
-cloudflared tunnel run --token "$COPYPARTY_CLOUDFLARED_TOKEN" --url http://127.0.0.1:8080
+cloudflared tunnel run --token "$COPYPARTY_CLOUDFLARED_TOKEN"
