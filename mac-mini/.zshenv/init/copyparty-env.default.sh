68d22ed5ce6255c35defac272331a6645ff90711
Gregory Linscheid <linscheid.greg@gmail.com>
2025-08-15 14:48:19 -0700

    :scissors: Simplify copyparty setup

--- mac-mini/.zshenv/init/copyparty-env.default.sh
+++ mac-mini/.zshenv/init/copyparty-env.default.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # If you have a real token, use this template to create an env file
-# and place it in: `/mac-mini/.zshenv/init/`
+# and place it in: `/mac-mini/.zshenv/init/copyparty-env.sh`
 
 export COPYPARTY_CLOUDFLARED_TOKEN=''
