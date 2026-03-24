eb247c2db1ba55f9eab719b604be8bbe247b9d81
Gregory Linscheid <linscheid.greg@gmail.com>
2026-03-23 18:30:39 -0700

    :bulb: pnpm completions

--- zsh/.zshenv/post/javascript.sh
+++ zsh/.zshenv/post/javascript.sh
@@ -5,6 +5,11 @@ export PNPM_HOME="/Users/greg.linscheid/Library/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 # pnpm end
 
+# pnpm zsh completions
+if command -v pnpm >/dev/null 2>&1; then
+  . <(pnpm completion zsh)
+fi
+
 # bun
 export BUN_INSTALL="$HOME/.bun"
 
