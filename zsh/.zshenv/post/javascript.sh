ea21093f0804bb2380e67edb88fae402a493c6d8
Gregory Linscheid <linscheid.greg@gmail.com>
2026-04-21 22:31:33 -0700

    :wrench: no pnpm

--- zsh/.zshenv/post/javascript.sh
+++ zsh/.zshenv/post/javascript.sh
@@ -5,11 +5,6 @@ export PNPM_HOME="/Users/greg.linscheid/Library/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 # pnpm end
 
-# pnpm zsh completions
-if command -v pnpm >/dev/null 2>&1; then
-  . <(pnpm completion zsh)
-fi
-
 # bun
 export BUN_INSTALL="$HOME/.bun"
 
