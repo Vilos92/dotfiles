dc2721ec00205da8c2289414e5ec64872bd467a4
Gregory Linscheid <linscheid.greg@gmail.com>
2026-07-23 21:52:26 -0700

    fix ssh-mini-hermes

--- remote/.zshenv/post/remote.sh
+++ remote/.zshenv/post/remote.sh
@@ -5,6 +5,7 @@ alias ssh-sparkify='ssh root@147.182.226.122'
 
 # Mac Mini (Must be on tailwind)
 alias ssh-mini='ssh greg.linscheid@gregs-mac-mini'
-# Interactive zsh so ~/.local/bin and homebrew land on PATH (set in .zshrc).
+# Interactive login zsh: -i for .zshrc (~/.local/bin on PATH), -l for
+# /etc/zprofile & .zprofile (docker/homebrew on PATH).
 # hermes attaches/creates the persistent "hermes" tmux session running the TUI.
-alias ssh-mini-hermes='ssh -t greg.linscheid@gregs-mac-mini "zsh -ic hermes"'
+alias ssh-mini-hermes='ssh -t greg.linscheid@gregs-mac-mini "zsh -ilc hermes"'
